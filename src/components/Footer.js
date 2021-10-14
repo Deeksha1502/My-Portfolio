156d5cb1b32217ed51d854b2318122153d3b31f2
Deeksha <deekshad1502@gmail.com>
2021-10-14 22:23:27 +0530

    changed text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react"
 const Footer=() =>{
     return (<div className="footer" id="footer">
 
-    <h4 className="links">Contact Me</h4>
+    <h3 className="links">Connect with me</h3>
     <a className="twitter" href="https://www.twitter.com/Deeksha__1502"><i  className="social-icon fab fa-twitter"></i></a>
     <a className="Github" href="https://github.com/Deeksha1502"><i className="social-icon fab fa-github"></i></a>
     <a className="linkedin" href="https://www.linkedin.com/in/deeksha-deepak-52009a203/"><i className="social-icon fab fa-linkedin"></i></a>
