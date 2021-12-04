6d1bc8925bdfa043eaf1ace1c8c068fd8fb6d304
Deeksha <deekshad1502@gmail.com>
2021-12-04 23:20:42 +0530

    text change

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,9 +5,9 @@ const Body=() =>
     return(
         <div className="container1" id="container1">
            <h4 className="heading"> About Me</h4>
-           <p className="text"> I completed my schooling from Presidency school,NLO and high school from Base Pu college,RNR.
-               <br />I love reading, learning new stuff and I strive to become a better person with each passing day. I am pursuing Computer Science Engineering at Bangalore Institute of Technology 
-               I am currently working as the co-ordinator of Editorial at IEDC:BIT
+           <p className="text"> I completed my schooling from Presidency school,Bangalore and high school from Base Pu college,Bangalore.
+               <br />I love reading, learning new things and I strive to become a better person with each passing day. I am pursuing Computer Science Engineering from Bangalore Institute of Technology and
+               I am currently working as the Co-ordinator of Editorial at IEDC:BIT
            </p>
         </div>
     );
